65080593565c5d4ecdd7c6f3e0f1d2a7b0bcda10
Dmitrii Kudriashov <kd20121989@gmail.com>
2022-04-08 20:38:24 +0400

    Assertions added. Need to make selector for birth days.

--- src/test/java/guru/qa/web/PracticeForm.java
+++ src/test/java/guru/qa/web/PracticeForm.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -22,17 +23,18 @@ public class PracticeForm {
         String firstName = "Dmitrii";
         String lastName = "Kudriashov";
         String userEmail = "somemail@google.com";
-        String userNumber = "9675915303";
+        String gender = "Male";
+        String phoneNumber = "9675915303";
         String yearOfBirth = "1989";
         String monthOfBirth = "December";
         String dayOfBirth = "20";
-        String dateOfBirth = dayOfBirth + " " + monthOfBirth + " " + yearOfBirth;
+        String dateOfBirth = dayOfBirth + " " + monthOfBirth + "," + yearOfBirth;
         String subjectEng = "English";
         String subjectMath = "Maths";
         String subjectCompSci = "Computer Science";
         String hobbyRead = "Reading";
         String hobbyMusic = "Music";
-        String fileName = "photo_2020-02-25_11-37-04 - Copy.jpg";
+        String pictureFile = "photo_2020-02-25_11-37-04 - Copy.jpg";
         String currentAddress = "13 Lightning Point Drive, Memphis";
         String stateUttar = "Uttar Pradesh";
         String cityLucknow = "Lucknow";
@@ -42,31 +44,47 @@ public class PracticeForm {
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(userEmail);
-        $(byTagAndText("label","Male")).click();
-        $("#userNumber").setValue(userNumber);
+        $(byTagAndText("label",gender)).click();
+        $("#userNumber").setValue(phoneNumber);
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOption(yearOfBirth);
         $(".react-datepicker__month-select").selectOption(monthOfBirth);
+//        $$(".react-datepicker__week").findBy(text(dayOfBirth)).click(); This is not working...
+//      $(byTagAndText("div",(monthOfBirth + " " + dayOfBirth + "th"))).click(); // This is not working
+//        $$(".react-datepicker__week").findBy(text("react-datepicker__day.react-datepicker__day--020")).click(); // This is not working
         $("#dateOfBirth > div.react-datepicker__tab-loop > div.react-datepicker-popper > div > div > " +
                 "div.react-datepicker__month-container > div.react-datepicker__month > div:nth-child(4) > " +
                 "div.react-datepicker__day.react-datepicker__day--020").click(); //This is disgusting but works...
-        //$(byTagAndText("div",(monthOfBirth + " " + dayOfBirth + "th"))).click(); // This is not working
         $("#subjectsInput").setValue(subjectMath.substring(0,1)).pressEnter()
                 .setValue(subjectEng.substring(0,1)).pressEnter()
-                .setValue(subjectCompSci.substring(0,1)).pressEnter();
+                .setValue(subjectCompSci.substring(0,3)).pressEnter();
         $(byTagAndText("label",hobbyRead)).click();
         $(byTagAndText("label",hobbyMusic)).click();
-        $("#uploadPicture").uploadFromClasspath(fileName);
+        $("#uploadPicture").uploadFromClasspath(pictureFile);
         $("#currentAddress").setValue(currentAddress);
         $(byTitle("Ad.Plus Advertising")).click();
         $("#state").scrollIntoView(true).click();
-        $(byTagAndText("div",stateUttar)).click();
+        $("#state").$(byTagAndText("div",stateUttar)).click();
         $("#city").click();
-        $(byTagAndText("div",cityLucknow)).click();
+        $("#city").$(byTagAndText("div",cityLucknow)).click();
         zoom(0.67);
         $("#submit").click();
 
         //Assertions
-        
+        $(".table-responsive").$(byText("Student Name")).sibling(0).shouldHave(text(firstName + " " + lastName));
+        $(".table-responsive").$(byText("Student Email")).sibling(0).shouldHave(text(userEmail));
+        $(".table-responsive").$(byText("Gender")).sibling(0).shouldHave(text(gender));
+        $(".table-responsive").$(byText("Mobile")).sibling(0).shouldHave(text(phoneNumber));
+        $(".table-responsive").$(byText("Date of Birth")).sibling(0).shouldHave(text(dateOfBirth));
+        $(".table-responsive").$(byText("Subjects")).sibling(0).shouldHave(text(
+                String.join(", ",subjectMath,subjectEng,subjectCompSci)));
+        $(".table-responsive").$(byText("Hobbies")).sibling(0).shouldHave(text(
+                String.join(", ",hobbyRead,hobbyMusic)));
+        $(".table-responsive").$(byText("Picture")).sibling(0).shouldHave(text(pictureFile));
+        $(".table-responsive").$(byText("Address")).sibling(0).shouldHave(text(currentAddress));
+        $(".table-responsive").$(byText("State and City")).sibling(0).shouldHave(text(stateUttar + " " + cityLucknow));
+        $(".modal-footer").$("#closeLargeModal").scrollIntoView(true).click();
+        $(".practice-form-wrapper").shouldBe(visible);
+
     }
 }
